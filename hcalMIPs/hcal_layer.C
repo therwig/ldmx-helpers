ec38341a9bc91ab50afd00b5c0802975975d8416
Christian Herwig <noreply@github.com>
2024-12-18 10:30:24 -0500

    add some example pltos

--- hcalMIPs/hcal_layer.C
+++ hcalMIPs/hcal_layer.C
@@ -7,9 +7,21 @@
 #include "TH3D.h"
 #include "TCanvas.h"
 
-void hcal_layer() {
+class TP {
+public:
+  int strip=-1;
+  int layer=-1;
+  float pe=0;
+  TP() {}
+  TP (int s, int l, float e) : strip(s), layer(l), pe(e) {}
+};
+int get_idx(int s, int l){
+  return s+100+l;
+}
+
+void runIt(TString fname="v3.3.6-1e_100PE_241008.root", TString procName="sim", TString oname="hist.root") {
     // Open the ROOT file
-    TFile* file = TFile::Open("events_10k.root", "READ");
+    TFile* file = TFile::Open(fname, "READ");
     if (!file || file->IsZombie()) {
         std::cerr << "Error opening file!" << std::endl;
         return;
@@ -24,59 +36,75 @@ void hcal_layer() {
 
     // Set up the TTreeReader
     TTreeReader reader(tree);
-    TTreeReaderArray<float> energy(reader, "HcalRecHits_test.energy_");
-    TTreeReaderArray<int>pdg(reader, "EcalScoringPlaneHits_test.pdgID_");
-    TTreeReaderArray<float>y_pos(reader, "HcalRecHits_test.ypos_");
-    TTreeReaderArray<float>z_pos(reader, "HcalRecHits_test.zpos_");
-    TTreeReaderArray<float>x_pos(reader, "HcalRecHits_test.xpos_");
-    TTreeReaderArray<float>z_ecal(reader, "EcalScoringPlaneHits_test.z_");
-    TTreeReaderArray<int>ecal_track(reader, "EcalScoringPlaneHits_test.trackID_");
-    TTreeReaderArray<int>hcal_track(reader, "HcalScoringPlaneHits_test.trackID_");
-    TTreeReaderArray<int>hcal_sec(reader, "HcalRecHits_test.section_"); 
-    TTreeReaderArray<int>hcal_strip(reader, "HcalRecHits_test.strip_"); 
-    TTreeReaderArray<int>hcal_layer(reader, "HcalRecHits_test.layer_");
-    TTreeReaderArray<float>e_ecal(reader, "EcalScoringPlaneHits_test.energy_"); 
+    TTreeReaderArray<float>truth_e(reader, "TargetScoringPlaneHits_"+procName+".energy_"); 
+    TTreeReaderArray<int>hcal_sec(reader, "HcalRecHits_"+procName+".section_"); 
+    TTreeReaderArray<int>hcal_strip(reader, "HcalRecHits_"+procName+".strip_"); 
+    TTreeReaderArray<int>hcal_layer(reader, "HcalRecHits_"+procName+".layer_");
+    TTreeReaderArray<float>hcal_pe(reader, "HcalRecHits_"+procName+".pe_"); 
 
-    TH3D* hist = new TH3D("hist", "Hcal Rec Hits", 100, -17000, 7000,100, -7000, 7000, 100, -10000, 14000);
-    TH1D* hist2 = new TH1D("hist2", "Hits in Hcal", 100, 0, 6);
-    TH2D* hist3 = new TH2D("hist3", "Strips and Layers", 5, 1, 6, 48, 1, 49);
-    while (reader.Next()) {
-        // Check that pdg and x_pos arrays have entries before processing
-        size_t pdgSize = pdg.GetSize();
-        size_t xPosSize = x_pos.GetSize();
-        size_t zEcalSize = z_ecal.GetSize();
-        size_t energySize = energy.GetSize();
-	int hits = 0;
-	int group = 0;
-	int layer = 0;
+    TFile* fo = new TFile(oname,"recreate");
+    TH1F* deepestTP = new TH1F("deepestTP",";layer of the deepest Hcal tp",51,-0.5,50.5);
+    TH1F* deepestNonIsoTP = new TH1F("deepestNonIsoTP",";layer of the deepest non-isolated Hcal tp",51,-0.5,50.5);
+    TH2F* deepestTP_vs_energy = new TH2F("deepestTP_vs_energy",";layer of the deepest Hcal tp;truth energy",51,-0.5,50.5,16,0,8e3);
     
-        // Loop through each entry within the arrays for this event
-        for (size_t i = 0; i < pdgSize; i++) {
-            if (pdg[i] == 13 && z_ecal[i] > 800 && e_ecal[i]/1000 <1) {
-	       for (size_t j =0; j < xPosSize; j++) {
-		   if (hcal_sec[j] ==0) {
-		      hits++;
-                      // Print each corresponding x_pos, energy, pdg, and z_ecal value
-                      std::cout << "Position: " << x_pos[j] 
-                                << " Energy: " << energy[j] 
-                                << " PDG code: " << pdg[i] 
-                                << " z_ecal: " << z_ecal[i] 
-                                << std::endl;
-		      //hist2->Fill(energy[j]);
-		      group = (hcal_strip[j]/8) % 5 + 1;
-		      layer = (hcal_layer[j]/2) % 48 + 1;
-		      std::cout << "Strip: ," << hcal_strip[j] << " Group: " << group << std::endl;
-		      hist3->Fill(group, layer);
-		   }
-	       }
-            }
-        }
-	hist3->GetXaxis()->SetTitle("Strip");
-	hist3->SetStats(kFALSE);
-	hist3->Draw("colz");
+    long iEvt=0;
+    while (reader.Next()) {
+      //if(iEvt>100) break;
+      iEvt++;
+      if(iEvt%1000==0) cout << "processing events " << iEvt << endl;
 
+      // build a list of TPs
+      map<int,TP> tp_list;
+      for (size_t i = 0; i < hcal_layer.GetSize(); i++) {
+	if (hcal_sec[i]!=0) continue; // only consider back HCal hits
+	if (hcal_pe[i]<50) continue; // only consider hits consistent with MIP energies
+	int tp_layer = (hcal_layer[i]/2)+1; // count from layer 1
+	int tp_strip = (hcal_strip[i]/8);
+	int tp_idx = get_idx(tp_strip, tp_layer);
+	if (tp_list.count(tp_idx)) tp_list[tp_idx].pe += hcal_pe[i];
+	else {
+	  TP tp(tp_strip, tp_layer, hcal_pe[i]);
+	  tp_list[tp_idx] = tp;
+	}
+      }
+      int lastLayer = 0;
+      int lastNonIsoLayer = 0;
+      for(auto tp_pair : tp_list){
+	auto tp = tp_pair.second;
+	bool isIsolated = true;
+	//look for a nearby TP
+	for(int s=tp.strip-1; s<=tp.strip+1; s++){
+	  if (s<0 || s>4) continue;
+	  for(int l=tp.layer-1; l<=tp.layer+1; l++){
+	    if (l<0 || l>48) continue;
+	    if (tp_list.count(get_idx(s,l))) isIsolated = false;
+	    if (!isIsolated) break;
+	  }
+	  if (!isIsolated) break;
+	}
+	if (tp.layer > lastLayer) lastLayer = tp.layer;
+	if (!isIsolated && tp.layer > lastNonIsoLayer) lastNonIsoLayer = tp.layer;
+      }
+      deepestTP->Fill(lastLayer);
+      deepestNonIsoTP->Fill(lastNonIsoLayer);
+      if (truth_e.GetSize()){
+	deepestTP_vs_energy->Fill(lastLayer, truth_e[0]);
+      }
     }
-
+    fo->Write();
+    fo->Close();
+    file->Close();
 }
+
+void hcal_layer(bool doBkg=false){
+  if(doBkg){
+    // run with the background file arguments
+    runIt("v3.3.6-1e_100PE_241008.root", "sim", "hist_bkg.root");
+  } else {
+    // run with the signal file arguments
+    runIt("muGun_1energy8.root", "test", "hist_sig.root");
+  }
+}
+
 
 
